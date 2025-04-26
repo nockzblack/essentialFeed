668a4f7a09bc6f69e6c475882b2f3dd23e9316b2
Fernando Benavides <fernando.benavides@oracle.com>
2025-04-26 15:42:55 -0600

    Add URLSessionHTTPClientTests

---
+++ EssentialFeed/EssentialFeedTests/Fedd API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,12 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeed
+//
+//  Created by Fernando Benavides on 26/04/25.
+//
+
+import XCTest
+
+class URLSessionHTTPClientTests: XCTestCase {
+    
+}
