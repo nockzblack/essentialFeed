5b945a16a5f19a8722c9e0fa65e56b7777cad754
Fernando Benavides <fernando.benavides@oracle.com>
2025-04-26 15:55:10 -0600

    Creates URLSession data task with URL on `URLSessionHTTPClient.get(from: URL)` [Subclass mocking approach]

--- EssentialFeed/EssentialFeedTests/Fedd API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Fedd API/URLSessionHTTPClientTests.swift
@@ -7,6 +7,42 @@
 
 import XCTest
 
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { _, _, _ in }
+    }
+}
+
+
 class URLSessionHTTPClientTests: XCTestCase {
     
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "http://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession, @unchecked Sendable {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+        
+        private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+            
+        }
+    }
 }
