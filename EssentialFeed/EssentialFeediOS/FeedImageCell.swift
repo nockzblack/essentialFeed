01d88ebbf59bd0313bd657712c6f09764a2cdaca
Fernando Benavides <fernando.benavides.contractor@realtor.com>
2025-08-26 12:23:01 -0600

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Fer Benavides Rodriguez on 26/08/25.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
