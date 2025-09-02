81dbc760bdc27dade4f14a2edaf7b2f6e7e61ad3
Fernando Benavides <fernando.benavides.contractor@realtor.com>
2025-09-02 14:46:02 -0600

    Move FeedImage value translations (including image loading state) to `FeedImageViewModel`. Now, the `FeedImageCellController` acts as a binder between the `View` and the `ViewModel`

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -8,46 +8,90 @@
 import UIKit
 import EssentialFeed
 
-final class FeedImageCellController {
+final class FeedImageViewModel {
     private var task: FeedImageDataLoaderTask?
     private let model: FeedImage
     private let imageLoader: FeedImageDataLoader
+    
+    var description: String? {
+        return model.description
+    }
+    var location: String?  {
+        return model.location
+    }
+    var hasLocation: Bool {
+        return location != nil
+    }
+    
+    var onImageLoad: ((UIImage) -> Void)?
+    var onShouldRetryImageLoadStateChange: ((Bool) -> Void)?
+    var onImageLoadingStateChange: ((Bool) -> Void)?
     
     init(model: FeedImage, imageLoader: FeedImageDataLoader) {
         self.model = model
         self.imageLoader = imageLoader
     }
     
-    func view() -> UITableViewCell {
-        let cell = FeedImageCell()
-        cell.locationContainer.isHidden = (model.location == nil)
-        cell.locationLabel.text = model.location
-        cell.descriptionLabel.text = model.description
-        cell.feedImageView.image = nil
-        cell.feedImageRetryButton.isHidden = true
-        cell.feedImageContainer.startShimmering()
-        let loadImage = { [weak self, weak cell] in
-            guard let self = self else { return }
-            
-            task = imageLoader.loadImageData(from: model.url) { [weak cell] result in
-                let data = try? result.get()
-                let image = data.map(UIImage.init) ?? nil
-                cell?.feedImageView.image = image
-                cell?.feedImageRetryButton.isHidden = (image != nil)
-                cell?.feedImageContainer.stopShimmering()
+    func loadImageData() {
+        onImageLoadingStateChange?(true)
+        onShouldRetryImageLoadStateChange?(false)
+        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
+            if let image = (try? result.get()).flatMap(UIImage.init) {
+                self?.onImageLoad?(image)
+            } else {
+                self?.onShouldRetryImageLoadStateChange?(true)
             }
+            self?.onImageLoadingStateChange?(false)
         }
-        
-        cell.onRetry = loadImage
-        loadImage()
+    }
+    
+    func cancelImageDataLoad() {
+        task?.cancel()
+        task = nil
+    }
+}
+
+final class FeedImageCellController {
+    
+    private let viewModel: FeedImageViewModel
+    
+    init(viewModel: FeedImageViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    func view() -> UITableViewCell {
+        let cell = binded(FeedImageCell())
+        viewModel.loadImageData()
         return cell
     }
     
     func preload() {
-        task = imageLoader.loadImageData(from: model.url) { _ in }
+        viewModel.loadImageData()
     }
     
     func cancelLoad() {
-        task?.cancel()
+        viewModel.cancelImageDataLoad()
+    }
+    
+    private func binded(_ cell: FeedImageCell) -> FeedImageCell {
+        cell.locationContainer.isHidden = !viewModel.hasLocation
+        cell.locationLabel.text = viewModel.location
+        cell.descriptionLabel.text = viewModel.description
+        cell.onRetry = viewModel.loadImageData
+        
+        
+        viewModel.onImageLoad = { [weak cell] image in
+            cell?.feedImageView.image = image
+        }
+        
+        viewModel.onImageLoadingStateChange = { [weak cell] isLoading in
+            cell?.feedImageContainer.isShimmering = isLoading
+        }
+        
+        viewModel.onShouldRetryImageLoadStateChange = { [weak cell] shouldRetry in
+            cell?.feedImageRetryButton.isHidden = !shouldRetry
+        }
+        
+        return cell
     }
 }
