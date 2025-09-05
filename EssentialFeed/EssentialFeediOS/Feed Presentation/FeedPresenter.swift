ab0999b613026612e050b4564da16d4d2b363136
Fernando Benavides <fernando.benavides.contractor@realtor.com>
2025-09-05 09:10:53 -0600

    Add `FeedPresenter` holding feed loading presentation ogic (loading state and laoded feed)

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,37 @@
+//
+//  FeedPresenter.swift
+//  EssentialFeed
+//
+//  Created by Fer Benavides Rodriguez on 04/09/25.
+//
+import EssentialFeed
+
+protocol FeedLoadingView: AnyObject {
+    func display(isLoading: Bool)
+}
+
+protocol FeedView {
+    func display(feed: [FeedImage])
+}
+
+final class FeedPresenter {
+    typealias Observer<T> = (T) -> Void
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    weak var loadingView: FeedLoadingView?
+    var feedView: FeedView?
+    
+    func loadFeed() {
+        loadingView?.display(isLoading: true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.feedView?.display(feed: feed)
+            }
+            self?.loadingView?.display(isLoading: false)
+        }
+    }
+}
