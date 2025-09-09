aa729e11cb8cabefa6ef579f8103166aa3f548be
Fernando Benavides <fernando.benavides.contractor@realtor.com>
2025-09-09 11:13:59 -0600

    Add FeedImagePresenter holding FeedImage presentation logic (including image loading state)

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedImageViewModel.swift
@@ -5,56 +5,14 @@
 //  Created by Fer Benavides Rodriguez on 02/09/25.
 //
 
-import Foundation
-import EssentialFeed
-
-final class FeedImageViewModel<Image> {
-    typealias Observer<T> = (T) -> Void
+struct FeedImageViewModel<Image> {
+    let description: String?
+    let location: String?
+    let image: Image?
+    let isLoading: Bool
+    let shouldRetry: Bool
     
-    private var task: FeedImageDataLoaderTask?
-    private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    private let imageTransformer: (Data) -> Image?
-    
-    var description: String? {
-        return model.description
-    }
-    var location: String?  {
-        return model.location
-    }
     var hasLocation: Bool {
         return location != nil
-    }
-    
-    var onImageLoad: Observer<Image>?
-    var onShouldRetryImageLoadStateChange: Observer<Bool>?
-    var onImageLoadingStateChange: Observer<Bool>?
-    
-    init(model: FeedImage, imageLoader: FeedImageDataLoader, imageTransformer: @escaping (Data) -> Image?) {
-        self.model = model
-        self.imageLoader = imageLoader
-        self.imageTransformer = imageTransformer
-    }
-    
-    func loadImageData() {
-        onImageLoadingStateChange?(true)
-        onShouldRetryImageLoadStateChange?(false)
-        task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            self?.handle(result)
-        }
-    }
-    
-    func cancelImageDataLoad() {
-        task?.cancel()
-        task = nil
-    }
-    
-    private func handle(_ result: FeedImageDataLoader.Result) {
-        if let image = (try? result.get()).flatMap(imageTransformer) {
-            onImageLoad?(image)
-        } else {
-            onShouldRetryImageLoadStateChange?(true)
-        }
-        onImageLoadingStateChange?(false)
     }
 }
