7859ab5a6ce4bfa6e1305936c3b3bb55c11b694d
Fernando Benavides <fernando.benavides.contractor@realtor.com>
2025-09-17 15:19:09 -0600

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,11 @@
+//
+//  ErrorView.swift
+//  EssentialFeed
+//
+//  Created by Fer Benavides Rodriguez on 17/09/25.
+//
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
