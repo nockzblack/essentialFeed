b74a42ddb22294391161659425254a7c7592b186
Fernando Benavides <fernando.benavides.contractor@realtor.com>
2025-09-17 14:52:40 -0600

    Does not render load feed error on load

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -12,8 +12,13 @@ protocol FeedViewControllerDelegate {
     func didRequestFeedRefresh()
 }
 
+public final class ErrorView: UIView {
+    public var message: String?
+}
+
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView {
     var delegate: FeedViewControllerDelegate?
+    public let errorView = ErrorView()
     var tableModel = [FeedImageCellController]() {
         didSet { tableView.reloadData() }
     }
--- EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewController+TestHelpers.swift
@@ -10,6 +10,10 @@ import UIKit
 import EssentialFeediOS
 
 extension FeedViewController {
+    var errorMessage: String? {
+        return errorView.message
+    }
+    
     var isShowingLoadingIndicator: Bool {
         refreshControl?.isRefreshing == true
     }
