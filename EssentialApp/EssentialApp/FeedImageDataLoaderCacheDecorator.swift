e547698a9663ad8964a3cebdc7ea13ff85f72671
nockzblack <nockzblack@gmail.com>
2026-01-13 22:40:42 -0600

    Create `saveIgnoringResult` method to clarify intent

--- EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderCacheDecorator.swift
@@ -26,3 +26,9 @@ public class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader  {
         }
     }
 }
+
+private extension FeedImageDataCache {
+    func saveIgnoringResult(_ data: Data, for url: URL) {
+        save(data, for: url) { _ in }
+    }
+}
