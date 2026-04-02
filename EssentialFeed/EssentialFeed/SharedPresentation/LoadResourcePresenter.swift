aaf6e57881022e803d341c055122f79cf93cd0dc
nockzblack <nockzblack@gmail.com>
2026-04-02 12:30:14 -0600

    Replace `FEED_VIEW_CONNECTION_ERROR` key with `GENERIC_CONNECTION_ERRO`

--- EssentialFeed/EssentialFeed/SharedPresentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/SharedPresentation/LoadResourcePresenter.swift
@@ -29,7 +29,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     }
     
     private var feedLoadError: String {
-        return NSLocalizedString("FEED_VIEW_CONNECTION_ERROR", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self), comment: "Error message displayed when we can't load the image feed from the server")
+        return NSLocalizedString("GENERIC_CONNECTION_ERROR", tableName: "Feed", bundle: Bundle(for: FeedPresenter.self), comment: "Error message displayed when we can't load the image feed from the server")
     }
     
     public func didStartLoading() {
@@ -42,7 +42,7 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
         resourceView.display(mapper(resource))
     }
     
-    public func didFinishLoadingFeed(with error: Error) {
+    public func didFinishLoading(with error: Error) {
         errorView.display(.error(message: feedLoadError))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
     }
