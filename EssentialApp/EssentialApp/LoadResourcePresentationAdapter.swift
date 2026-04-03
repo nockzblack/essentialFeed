aeaaee0131282dd6369a350d50463b1887f716fe
nockzblack <nockzblack@gmail.com>
2026-04-03 17:20:57 -0600

    Make Presentation Adapater generic so it can be reused

---
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -0,0 +1,44 @@
+//
+//  LoadResourcePresentationAdapter.swift
+//  EssentialFeed
+//
+//  Created by Fer Benavides Rodriguez on 21/01/26.
+//
+
+import Combine
+import EssentialFeed
+import EssentialFeediOS
+
+final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
+    private let loader: () -> AnyPublisher<Resource, Error>
+    private var cancellable: Cancellable?
+    var presenter: LoadResourcePresenter<Resource, View>?
+    
+    init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
+        self.loader = loader
+    }
+    
+    func loadResource() {
+        presenter?.didStartLoading()
+        
+        cancellable = loader()
+            .dispatchOnMainQueue()
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    switch completion {
+                    case .finished: break
+                        
+                    case let .failure(error):
+                        self?.presenter?.didFinishLoading(with: error)
+                    }
+                }, receiveValue: { [weak self] feed in
+                    self?.presenter?.didFinishLoading(with: feed)
+                })
+    }
+}
+
+extension LoadResourcePresentationAdapter: FeedViewControllerDelegate {
+    func didRequestFeedRefresh() {
+        loadResource()
+    }
+}
