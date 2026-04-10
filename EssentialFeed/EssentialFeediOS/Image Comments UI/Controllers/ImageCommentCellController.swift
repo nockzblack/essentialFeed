ca5de70d44e4867f6d56776a16693b64e6485c54
nockzblack <nockzblack@gmail.com>
2026-04-10 09:33:42 -0600

    Move snapshot test helpers to its own file

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,10 @@
+//
+//  ImageCommentCeelController.swift
+//  EssentialFeed
+//
+//  Created by Fer Benavides Rodriguez on 10/04/26.
+//
+
+import UIKit
+
+public class ImageCommentCeelController
---
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  EssentialFeed
+//
+//  Created by Fer Benavides Rodriguez on 09/04/26.
+//
+
