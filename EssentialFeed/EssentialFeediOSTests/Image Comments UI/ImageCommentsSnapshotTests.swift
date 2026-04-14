0bd4488fc7f06b0d901e8692d0d618aafc440c55
nockzblack <nockzblack@gmail.com>
2026-04-14 08:36:11 -0600

    Replace hardcoded fonts with dynamic fonts

--- EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Image Comments UI/ImageCommentsSnapshotTests.swift
@@ -18,6 +18,7 @@ class ImageCommentsSnapshotTests: XCTestCase {
         
         assert(snapshot: sut.snapshot(for: .iPhone17(style: .light)), named: "IMAGE_COMMENT_light")
         assert(snapshot: sut.snapshot(for: .iPhone17(style: .dark)), named: "IMAGE_COMMENT_dark")
+        assert(snapshot: sut.snapshot(for: .iPhone17(style: .light, contentSize: .extraExtraExtraLarge)), named: "IMAGE_COMMENT_light_extraExtraExtraLarge")
     }
     
     // MARK: - Helpers
