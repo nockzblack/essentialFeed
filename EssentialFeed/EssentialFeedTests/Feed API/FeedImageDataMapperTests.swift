4fcd07c3b50b447b21885fcd7be77bb4f34d045c
nockzblack <nockzblack@gmail.com>
2026-05-06 16:34:34 -0600

    Test FeedImageDataMapper in isolation

---
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedImageDataMapperTests.swift
@@ -0,0 +1,39 @@
+//
+//  FeedImageDataMapperTests.swift
+//  EssentialFeed
+//
+//  Created by Fer Benavides Rodriguez on 06/05/26.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedImageDataMapperTests: XCTestCase {
+    
+    func test_map_throwsErrorOnNon200HTTPResponse() throws {
+        let samples = [199, 201, 300, 400, 500]
+        
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try FeedImageDataMapper.map(anyData(), from: HTTPURLResponse(statusCode: code))
+            )
+        }
+    }
+    
+    func test_map_deliversInvalidDataErrorOn200HTTPResponseWithEmptyData() {
+        let emptyData = Data()
+        
+        XCTAssertThrowsError(
+            try FeedImageDataMapper.map(emptyData, from: HTTPURLResponse(statusCode: 200))
+        )
+    }
+    
+    func test_map_deliversReceivedNonEmptyDataOn200HTTResponse() throws {
+        let nonEmptyData = Data("non-empty Data".utf8)
+        
+        let result = try FeedImageDataMapper.map(nonEmptyData, from: HTTPURLResponse(statusCode: 200))
+        
+        XCTAssertEqual(result, nonEmptyData)
+    }
+    
+}
