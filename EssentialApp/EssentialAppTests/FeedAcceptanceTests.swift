512bf95015942ece79bfb4dc7101e4e93b65cf69
nockzblack <nockzblack@gmail.com>
2026-05-29 15:29:27 -0600

    Make in memory FeedStore implementation sync

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -168,18 +168,16 @@ final class FeedAcceptanceTests: XCTestCase {
             self.feedCache = feedCache
         }
         
-        func deleteCacheFeed(completion: @escaping DeletionCompletion) {
+        func deleteCacheFeed() throws {
             feedCache = nil
-            completion(.success(()))
         }
         
-        func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
             feedCache = CachedFeed(feed: feed, timestamp: timestamp)
-            completion(.success(()))
         }
         
-        func retrieve(completion: @escaping RetrievalCompletion) {
-            completion(.success(feedCache))
+        func retrieve() throws -> CachedFeed? {
+            feedCache
         }
         
         func insert(_ data: Data, for url: URL) throws {
